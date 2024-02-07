042aa03a5db875fc0d9ab682f96da6442d6db064
vivignutz <vgnutzmann@gmail.com>
2024-02-07 08:00:26 +0000

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import PetList from './PetList';
-import Header from './Header';
-import Footer from './Footer';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import PetList from "./PetList";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./App.css";
 
 function App() {
+  // Initial state of pets list
+  const [pets, setPets] = useState([]);
+
+  // Function to get pets from server
+  const fetchPets = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/pets');
+      setPets(response.data);
+    } catch (error) {
+      console.error('Error to get pets data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPets();
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <PetList />
+      <PetList pets={pets} setPets={setPets} />
       <Footer />
     </div>
   );
